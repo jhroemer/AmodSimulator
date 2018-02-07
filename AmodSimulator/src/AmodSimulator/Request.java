4f35155562f22a993f5995c8dea0a18b21802267
jenr <jenr@itu.dk>
2018-02-07 13:37:54 +0100

    todo's and some tests

--- AmodSimulator/src/AmodSimulator/Request.java
+++ AmodSimulator/src/AmodSimulator/Request.java
@@ -2,10 +2,12 @@ package AmodSimulator;
 
 import org.graphstream.graph.Node;
 
+// todo: in Fagnant & Kockelman requests also have a departure time
 public class Request {
     private int id;
     private Node origin;
     private Node destination;
+    // todo: private int departureTime; ??
 
     public Request(int id, Node location, Node destination) {
         this.id = id;
