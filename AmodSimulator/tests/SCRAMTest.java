16e6cdba65076bfb9e834ee552c4032613410a76
jenr <jenr@itu.dk>
2018-03-12 10:58:10 +0100

    added index scram test 3

--- AmodSimulator/tests/SCRAMTest.java
+++ AmodSimulator/tests/SCRAMTest.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class SCRAMTest {
 
     @Test
-    public void SCRAMTest1() {
+    public void objectBasedSCRAMTest1() {
         Graph graph = new MultiGraph("Graph #1");
         graph.setAutoCreate(true);
         graph.setStrict(false);
@@ -88,7 +88,7 @@ public class SCRAMTest {
     }
 
     @Test
-    public void IndexBasedSCRAMTest1() {
+    public void indexBasedScramTest1() {
         Graph graph = new MultiGraph("Graph #1");
         graph.setAutoCreate(true);
         graph.setStrict(false);
@@ -154,7 +154,7 @@ public class SCRAMTest {
     }
 
     @Test
-    public void SCRAMTest2() {
+    public void objectBasedSCRAMTest2() {
         Graph graph = new MultiGraph("Graph #2");
         graph.setAutoCreate(true);
         graph.setStrict(false);
@@ -292,7 +292,7 @@ public class SCRAMTest {
 
 
     @Test
-    public void SCRAMTest3() {
+    public void objectBasedSCRAMTest3() {
         Graph graph = new MultiGraph("Graph #3");
         graph.setAutoCreate(true);
         graph.setStrict(false);
@@ -349,6 +349,63 @@ public class SCRAMTest {
         Assert.assertEquals(r2, sortedAssignmentList.get(1).getEndNode());
         Assert.assertEquals(v1, sortedAssignmentList.get(2).getStartNode());
         Assert.assertEquals(r3, sortedAssignmentList.get(2).getEndNode());
+    }
+
+    @Test
+    public void indexBasedSCRAMTest3() {
+        Graph graph = new MultiGraph("Graph #3");
+        graph.setAutoCreate(true);
+        graph.setStrict(false);
+
+        graph.addEdge("v1r1", "v1", "r1").setAttribute("layout.weight", 1);
+        graph.addEdge("v1r2", "v1", "r2").setAttribute("layout.weight", 5);
+        graph.addEdge("v1r3", "v1", "r3").setAttribute("layout.weight", 9);
+        graph.addEdge("v2r1", "v2", "r1").setAttribute("layout.weight", 3);
+        graph.addEdge("v2r2", "v2", "r2").setAttribute("layout.weight", 7);
+        graph.addEdge("v2r3", "v2", "r3").setAttribute("layout.weight", 10);
+        graph.addEdge("v3r1", "v3", "r1").setAttribute("layout.weight", 13);
+        graph.addEdge("v3r2", "v3", "r2").setAttribute("layout.weight", 15);
+        graph.addEdge("v3r3", "v3", "r3").setAttribute("layout.weight", 17);
+//        Utility.setDistances(graph);
+
+        graph.getNode("v1").setAttribute("distTor1", 1);
+        graph.getNode("v1").setAttribute("distTor2", 4);
+        graph.getNode("v1").setAttribute("distTor3", 6);
+        graph.getNode("v2").setAttribute("distTor1", 2);
+        graph.getNode("v2").setAttribute("distTor2", 5);
+        graph.getNode("v2").setAttribute("distTor3", 5000);
+        graph.getNode("v3").setAttribute("distTor1", 3);
+        graph.getNode("v3").setAttribute("distTor2", 2000);
+        graph.getNode("v3").setAttribute("distTor3", 3500);
+
+        Vehicle v1 = new Vehicle("v1", graph.getNode("v1"));
+        Vehicle v2 = new Vehicle("v2", graph.getNode("v2"));
+        Vehicle v3 = new Vehicle("v3", graph.getNode("v3"));
+        List<Node> vehicleList = new ArrayList<>();
+        vehicleList.add(v1);
+        vehicleList.add(v2);
+        vehicleList.add(v3);
+
+        Request r1 = new Request(1, graph.getNode("r1"), null, 0);
+        Request r2 = new Request(2, graph.getNode("r2"), null, 0);
+        Request r3 = new Request(3, graph.getNode("r3"), null, 0);
+        List<Node> requestList = new ArrayList<>();
+        requestList.add(r1);
+        requestList.add(r2);
+        requestList.add(r3);
+
+        IndexBasedSCRAM s = new IndexBasedSCRAM(vehicleList, requestList);
+        Assert.assertEquals(6, s.getLongestEdgeWeight());
+
+        List<Edge> sortedAssignmentList = new ArrayList<>(s.getAssignments());
+        Collections.sort(sortedAssignmentList);
+
+        Assert.assertEquals(2, sortedAssignmentList.get(0).getStartIndex());
+        Assert.assertEquals(0, sortedAssignmentList.get(0).getEndIndex());
+        Assert.assertEquals(1, sortedAssignmentList.get(1).getStartIndex());
+        Assert.assertEquals(1, sortedAssignmentList.get(1).getEndIndex());
+        Assert.assertEquals(0, sortedAssignmentList.get(2).getStartIndex());
+        Assert.assertEquals(2, sortedAssignmentList.get(2).getEndIndex());
     }
 
     @Test
